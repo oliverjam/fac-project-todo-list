ad488244717a5ad671b3e06960f6e23755229a51
oliverjam <oliverjamesphillips@gmail.com>
2020-03-12 13:58:56 +0000

    Implement marking tasks complete feature

--- todo.js
+++ todo.js
@@ -1,10 +1,12 @@
 const todoForm = document.querySelector("#todoForm");
 const todoList = document.querySelector("#todoList");
+const todoTemplate = document.querySelector("#todoItem");
 
 todoForm.addEventListener("submit", event => {
   event.preventDefault();
   const newTask = event.target.elements.newTodo.value;
-  const listItem = document.createElement("li");
-  listItem.textContent = newTask;
-  todoList.appendChild(listItem);
+  const dom = todoTemplate.content.cloneNode(true);
+  const label = dom.querySelector("span");
+  label.textContent = newTask;
+  todoList.appendChild(dom);
 });
