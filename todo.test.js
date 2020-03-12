a59bd6509b90b726448fe2ff6104702c8c5da625
oliverjam <oliverjamesphillips@gmail.com>
2020-03-12 13:36:10 +0000

    Write failing test for adding tasks feature

--- todo.test.js
+++ todo.test.js
@@ -1,5 +1,15 @@
 test("Submitting a new task adds it to the list", t => {
-  // test goes here
+  const todoInput = document.querySelector("#newTodo");
+  todoInput.value = "test thing to do"; // type in our task text
+  const addTodoButton = document.querySelector("#addTodo");
+  addTodoButton.click(); // submit the add todo form
+  const todoList = document.querySelector("#todoList");
+  const todoItem = todoList.firstElementChild;
+  t.equal(todoItem.textContent, "test thing to do"); // our task should be inside the todo list
+
+  // reset DOM so we don't impact other tests
+  todoInput.value = "";
+  todoList.innerHTML = "";
 });
 
 test("Checking an entry marks it as complete", t => {
